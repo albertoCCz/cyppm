014d00751287dd07f777cebb81306e836ee50514
albertoCCz <albertocalzada95@gmail.com>
2022-07-29 21:14:00 +0200

    Add specific functions to save image buffer to ppm and jpeg formats

--- ppm_gen.pyx
+++ ppm_gen.pyx
@@ -4,13 +4,15 @@ from libc.stdio cimport FILE, fopen, fclose, fputs, sprintf
 import array
 from random import randint
 
+from PIL import Image
+
 cdef size_t WIDTH = 400
 cdef size_t HEIGHT = 400
 cdef size_t N_CHANNELS = 3
 cdef size_t IMAGE_SIZE = WIDTH * HEIGHT * N_CHANNELS
 
-cdef size_t MAX_COLOR_VALUE = 255
-cdef size_t MIN_COLOR_VALUE = 0
+cdef unsigned char MAX_COLOR_VALUE = 255
+cdef unsigned char MIN_COLOR_VALUE = 0
 
 cdef fputs_header(FILE *f):
     header_str = bytes(f"P3\n{WIDTH} {HEIGHT} {MAX_COLOR_VALUE}\n", 'UTF-8')
@@ -57,19 +59,30 @@ cdef dump_image(unsigned char[:,:,:] image, FILE *f):
 
             if col == (WIDTH - 1): fputs("\n", f)
 
-cpdef main():
+cdef to_ppm(unsigned char[:,:,:] image, char *file_name):
     # open the file
-    cdef const char *output_file_name = "output.ppm"
-    cdef FILE *f = fopen(output_file_name, "w")
+    cdef FILE *f = fopen(file_name, "w")
 
     # write the header
     fputs_header(f)
-
-    # gen image
-    cdef unsigned char[:,:,:] image = gen_image()
 
     # dump image to file
     dump_image(image, f)
 
     # close file
     fclose(f)
+
+cdef to_jpeg(unsigned char[:,:,:] image, file_name):
+    # read from buffer
+    image_jpeg = Image.frombuffer("RGB", (WIDTH, HEIGHT), image, "raw", "RGB", 0, 1)
+
+    # dump image to file
+    image_jpeg.save(file_name)
+
+cpdef main():
+    # gen image
+    cdef unsigned char[:,:,:] image = gen_image()
+
+    # save to file
+    file_name = "output.jpeg"
+    to_jpeg(image, file_name)
